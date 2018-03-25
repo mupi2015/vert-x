8ab8958fdef8289173b90e0b21c78743b29bf214
arkaprovob <arkaprovo.bhattacharjee@sath.com>
2018-03-26 00:25:05 +0530

    copied file from and existing project, and unsuccessful attempt 1

--- src/main/java/experiment/EntryPoint.java
+++ src/main/java/experiment/EntryPoint.java
@@ -1,9 +1,27 @@
 package experiment;
 
-public class EntryPoint {
+import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Future;
+import io.vertx.core.Vertx;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class EntryPoint extends AbstractVerticle {
 
 
     public static void main(String[] args) {
+        Vertx vertx = Vertx.vertx();
+        vertx.deployVerticle(new EntryPoint());
+    }
 
+
+    @Override
+    public void start(Future<Void> future) {
+        log.info("Welcome to Vert-x");
+    }
+
+    @Override
+    public void stop() {
+        log.info("Shutting down application");
     }
 }
